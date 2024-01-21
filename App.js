60c5b45dd61992925bd73c517fa182cec2863bad
duyquoc1402 <quocdndpd09548@fpt.edu.vn>
2024-01-21 16:16:05 +0700

    lab 1

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Text} from 'react-native';
+import MyLab3 from './src/screens/lab 3';
+
+const MyLab3 = () => {
+  return <MyLab3/>;
+};
+
+export default MyLab3;
